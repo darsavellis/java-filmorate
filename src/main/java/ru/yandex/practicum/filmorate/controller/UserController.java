2a5c18efbdbbc0ef5963c9487298a9ebf04cda8a
Aleksandr Darsavelidze <darsavellis@yandex.ru>
2024-05-30 14:06:18 +0300

    Реализован функционал по ТЗ 10 спринта. Реализованы модели, сервисы, контроллеры.

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
+
+import java.util.Collection;
+
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@RequestMapping("/users")
+public class UserController {
+    final UserService userService;
+
+    @GetMapping
+    public Collection<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User film) {
+        return userService.updateUser(film);
+    }
+}
