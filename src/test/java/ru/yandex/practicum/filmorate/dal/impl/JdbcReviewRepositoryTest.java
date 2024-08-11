e0a793498a4dd471602b954881cdc3ce597a7f79
Ivan Hamitsev <hamitsev-ie@yandex.ru>
2024-08-11 20:06:02 +0300

    Добавление интеграционных тестов репозитория Review

---
+++ src/test/java/ru/yandex/practicum/filmorate/dal/impl/JdbcReviewRepositoryTest.java
@@ -0,0 +1,133 @@
+package ru.yandex.practicum.filmorate.dal.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import ru.yandex.practicum.filmorate.dal.FilmRepository;
+import ru.yandex.practicum.filmorate.dal.ReviewRepository;
+import ru.yandex.practicum.filmorate.dal.UserRepository;
+import ru.yandex.practicum.filmorate.dal.impl.mappers.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@JdbcTest
+@AutoConfigureTestDatabase
+@Import({JdbcReviewRepository.class, JdbcUserRepository.class, JdbcFilmRepository.class, UserRowMapper.class,
+        FriendshipRowMapper.class, GenreRowMapper.class, FilmRowMapper.class, ReviewRowMapper.class})
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class JdbcReviewRepositoryTest {
+
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+    private final FilmRepository filmRepository;
+
+    @Test
+    public void createUpdateDeleteReview() {
+        User user1 = userRepository.save(LibraryForCreatingEntities.getUser(1));
+        User user2 = userRepository.save(LibraryForCreatingEntities.getUser(2));
+        Film film1 = filmRepository.save(LibraryForCreatingEntities.getFilm(1));
+        Film film2 = filmRepository.save(LibraryForCreatingEntities.getFilm(2));
+
+        Review review1 = reviewRepository.createReview(LibraryForCreatingEntities
+                .getReview(user1.getId(), film1.getId()));
+        Review review2 = reviewRepository.createReview(LibraryForCreatingEntities
+                .getReview(user2.getId(), film2.getId()));
+
+        assertNotNull(review1);
+        assertNotNull(review2);
+
+        Optional<Review> receivedReview = reviewRepository.getReviewById(review1.getReviewId());
+
+        assertThat(receivedReview)
+                .isPresent()
+                .hasValueSatisfying(review ->
+                        assertThat(review).hasFieldOrPropertyWithValue("content", review1.getContent()))
+                .hasValueSatisfying(review ->
+                        assertThat(review).hasFieldOrPropertyWithValue("userId", review1.getUserId()))
+                .hasValueSatisfying(review ->
+                        assertThat(review).hasFieldOrPropertyWithValue("filmId", review1.getFilmId()))
+                .hasValueSatisfying(review ->
+                        assertThat(review).hasFieldOrPropertyWithValue("isPositive", review1.getIsPositive()))
+                .hasValueSatisfying(review ->
+                        assertThat(review).hasFieldOrPropertyWithValue("reviewId", review1.getReviewId()))
+                .hasValueSatisfying(review ->
+                        assertThat(review).hasFieldOrPropertyWithValue("useful", review1.getUseful()));
+
+        review1.setIsPositive(false);
+        review1.setContent("Another content");
+        review1.setUserId(user2.getId());
+        review1.setFilmId(film2.getId());
+
+        receivedReview = Optional.of(reviewRepository.updateReview(review1));
+        assertThat(receivedReview).isPresent();
+        assertEquals(review1.getIsPositive(), receivedReview.get().getIsPositive());
+        assertEquals(review1.getContent(), receivedReview.get().getContent());
+        assertEquals(review1.getUserId(), receivedReview.get().getUserId());
+        assertEquals(review1.getFilmId(), receivedReview.get().getFilmId());
+
+        List<Review> reviewList = reviewRepository.getAllReviews(10);
+        assertEquals(2, reviewList.size(), "Оба созданных ревью ожидаются в листе");
+
+        reviewList = reviewRepository.getAllReviews(1);
+        assertEquals(1, reviewList.size(), "В листе ожидается один элемент");
+
+        reviewRepository.deleteReview(review1.getReviewId());
+        reviewList = reviewRepository.getAllReviews(10);
+        assertEquals(1, reviewList.size(), "В листе ожидается один элемент");
+
+        reviewRepository.deleteReview(review2.getReviewId());
+        reviewList = reviewRepository.getAllReviews(10);
+        assertEquals(0, reviewList.size(), "Ожидается пустой лист");
+    }
+
+    @Test
+    public void testUsefulReview() {
+        User user1 = userRepository.save(LibraryForCreatingEntities.getUser(1));
+        User user2 = userRepository.save(LibraryForCreatingEntities.getUser(2));
+        Film film1 = filmRepository.save(LibraryForCreatingEntities.getFilm(1));
+        Film film2 = filmRepository.save(LibraryForCreatingEntities.getFilm(2));
+
+        Review review1 = reviewRepository.createReview(LibraryForCreatingEntities
+                .getReview(user1.getId(), film1.getId()));
+        Review review2 = reviewRepository.createReview(LibraryForCreatingEntities
+                .getReview(user2.getId(), film2.getId()));
+
+        assertNotNull(review1);
+        assertNotNull(review2);
+
+        Optional<Review> receivedReview = reviewRepository.getReviewById(review1.getReviewId());
+        assertThat(receivedReview).isPresent();
+        long oldUseful = review1.getUseful();
+        reviewRepository.setLikeReview(review1.getReviewId(), user1.getId(), true);
+        receivedReview = reviewRepository.getReviewById(review1.getReviewId());
+        assertThat(receivedReview).isPresent();
+        long newUseful = receivedReview.get().getUseful();
+        assertEquals(oldUseful + 1, newUseful, "Ожидается увеличение полезности");
+
+        oldUseful = receivedReview.get().getUseful();
+        // повторный лайк не должен увеличить рейтинг
+        reviewRepository.setLikeReview(review1.getReviewId(), user1.getId(), true);
+        receivedReview = reviewRepository.getReviewById(review1.getReviewId());
+        assertThat(receivedReview).isPresent();
+        newUseful = receivedReview.get().getUseful();
+        assertEquals(oldUseful, newUseful, "Ожидается полезность на прежнем уровне");
+
+        oldUseful = receivedReview.get().getUseful();
+        reviewRepository.setLikeReview(review1.getReviewId(), user2.getId(), false);
+        receivedReview = reviewRepository.getReviewById(review1.getReviewId());
+        assertThat(receivedReview).isPresent();
+        newUseful = receivedReview.get().getUseful();
+        assertEquals(oldUseful - 1, newUseful, "Ожидается снижение полезности");
+    }
+}
--- src/test/java/ru/yandex/practicum/filmorate/dal/impl/JdbcUserRepositoryTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dal/impl/JdbcUserRepositoryTest.java
@@ -1,46 +1,38 @@
-package ru.yandex.practicum.filmorate.repository;
+package ru.yandex.practicum.filmorate.dal.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
-import ru.yandex.practicum.filmorate.dal.impl.JdbcUserRepository;
+import ru.yandex.practicum.filmorate.dal.impl.mappers.FilmRowMapper;
 import ru.yandex.practicum.filmorate.dal.impl.mappers.FriendshipRowMapper;
+import ru.yandex.practicum.filmorate.dal.impl.mappers.GenreRowMapper;
 import ru.yandex.practicum.filmorate.dal.impl.mappers.UserRowMapper;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.LocalDate;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @JdbcTest
-@Import({JdbcUserRepository.class, UserRowMapper.class,FriendshipRowMapper.class})
+@AutoConfigureTestDatabase
+@Import({JdbcUserRepository.class, JdbcFilmRepository.class, JdbcLikeRepository.class, FilmRowMapper.class,
+        UserRowMapper.class, GenreRowMapper.class, FriendshipRowMapper.class})
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class JdbcUserRepositoryTest {
-    public static final long TEST_USER_ID = 1L;
     private final JdbcUserRepository userRepository;
-
-    static User getUser() {
-        User user = new User();
-        user.setId(TEST_USER_ID);
-        user.setEmail("ivanov@email.com");
-        user.setName("Ivan");
-        user.setBirthday(LocalDate.of(1998, 10, 20));
-        user.setLogin("diamond");
-        return user;
-    }
 
     @Test
     public void testFindUserById() {
-        Optional<User> userOptional = userRepository.findById(TEST_USER_ID);
-
+        User user1 = userRepository.save(LibraryForCreatingEntities.getUser(1));
+        Optional<User> userOptional = userRepository.findById(user1.getId());
         assertThat(userOptional)
                 .isPresent()
                 .get()
                 .usingRecursiveComparison()
                 .ignoringExpectedNullFields()
-                .isEqualTo(getUser());
+                .isEqualTo(user1);
     }
 }
